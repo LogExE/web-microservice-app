d883cd892c12ba3d6b035c61af0237b44ebf36b6
LogXx <awesome149712@gmail.com>
2025-06-03 15:34:27 +0400

    Make scripts better

--- scripts/send_box.sh
+++ scripts/send_box.sh
@@ -1,3 +1,11 @@
-set -x
 
-curl -X POST localhost:8080/box -d '{"content": "test content @daily_cnt", "author": "author?? probably me"}'
+echo "Sending example boxes to REST service (and showing responses):"
+curl -X POST localhost:8080/box -d '{"content": "test content 123", "author": "author?? probably me"}'
+echo
+curl -X POST localhost:8080/box -d '{"content": "Haste make waste.", "author": "Konq Giu"}'
+echo
+curl -X POST localhost:8080/box -d '{"content": "https://some-spammy-site.com", "author": "the hax0r"}'
+echo
+
+echo "Boxes we have:"
+curl localhost:8080/boxes | jq .
