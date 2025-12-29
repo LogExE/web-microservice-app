216cbd13bad14e794677036a1a4de7e32b463b59
LogXx <awesome149712@gmail.com>
2025-12-29 20:03:46 +0400

    move port to pod

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
-podman kube play pod.yaml --publish 8080:8080
+podman kube play pod.yaml
+
 podman exec web-go-broker /opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic newBoxes
 podman exec web-go-broker /opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic likeBoxes
